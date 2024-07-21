a321f5a947d4569ff7ac1a6ba891297355d14e70
Swati01012002 <swati2111010@akgec.ac.in>
2024-07-22 02:20:17 +0530

    Model completed

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
-MIN_BET =1
+MIN_BET = 1
 
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column =[]
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+
+    return columns
+
+def print_slot(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) -1:
+                print(column[row], end=" | ")
+            else: 
+                print(column[row], end="")
+        print()
+
+    
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -57,6 +97,9 @@ def main():
     
     print(f"You are betting ${bet} on ${lines} lines. Total bet is equal to: ${total_bet}")
 
-
 main()
+slots = get_spin(ROWS, COLS, symbol_count)
+print_slot(slots)
+
+
 
